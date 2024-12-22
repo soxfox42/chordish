863c32e5ff3f243f541a4b2eaee5afd3d6b2d450
Finn Coffey <finn.coffey@gmail.com>
2024-12-22 15:44:51 +1100

    Allow chords to be found with either # or b
    Restructure chord data to make transposition possible in future

--- chords.typ
+++ chords.typ
@@ -5,6 +5,18 @@
   custom-chords.update(c => c + ((name): frets))
 }
 
+#let chord-regex = regex("^\\^?([A-G])([#b])?(.*)$")
+
+#let keys = (
+  "C": 0,
+  "D": 2,
+  "E": 4,
+  "F": 5,
+  "G": 7,
+  "A": 9,
+  "B": 11,
+)
+
 #let get-chord(name) = {
   if type(name) == content {
     name = name.text
@@ -13,5 +25,20 @@
   if name in custom-chords.get() {
     return custom-chords.get().at(name)
   }
-  built-in.at(state("instrument").get()).at(name, default: none)
+
+  let match = name.match(chord-regex)
+  if match == none {
+    return none
+  }
+
+  let (key, accidental, kind) = match.captures
+  let semitones = keys.at(key)
+  if accidental == "#" {
+    semitones += 1
+  } else if accidental == "b" {
+    semitones -= 1
+  }
+  semitones = calc.rem-euclid(semitones, 12)
+
+  built-in.at(state("instrument").get()).at(semitones).at(kind)
 }
--- chords/generate_chords.py
+++ chords/generate_chords.py
@@ -18,34 +18,57 @@ SUFFIXES = {
 
 instruments = {}
 
+# Based on a root of C, like MIDI.
+# The choice is basically arbitrary though.
+semitones = {
+    "C": 0,
+    "D": 2,
+    "E": 4,
+    "F": 5,
+    "G": 7,
+    "A": 9,
+    "B": 11,
+}
+
 FILES = ["guitar", "ukulele"]
 
 for file in FILES:
     with open(f"{file}.json") as f:
         data = json.load(f)
 
-    chords = {}
-    for group in data["chords"].values():
+    chords = [{} for _ in range(12)] 
+    for key, group in data["chords"].items():
+        index = semitones[key[0]]
+        if "sharp" in key or "#" in key:
+            index += 1
+        if "flat" in key or "b" in key:
+            index -= 1
+        index %= 12
+
         used_suffixes = []
         for chord in group:
             if chord["suffix"] not in SUFFIXES:
                 continue
             used_suffixes.append(chord["suffix"])
-            name = chord["key"] + SUFFIXES[chord["suffix"]]
+            suffix = SUFFIXES[chord["suffix"]]
             frets = chord["positions"][0]["frets"]
             frets = [str(fret) if fret != -1 else "x" for fret in frets]
-            chords[name] = "".join(frets)
+            chords[index][suffix] = "".join(frets)
         if len(used_suffixes) != len(SUFFIXES):
             print("Warning, unmatched suffixes")
     
     instruments[file] = chords
 
 
-with open("../songbook/chords.typ", "w") as f:
+with open("../chord-definitions.typ", "w") as f:
+    f.write("// Structure: chords > instrument > semitones-from-c > suffix\n")
     f.write("#let chords = (\n")
     for instrument, chords in instruments.items():
-        f.write(f'    {instrument}: (\n')
-        for name, frets in chords.items():
-            f.write(f'        "{name}": "{frets}",\n')
-        f.write(f'    ),\n')
+        f.write(f'  {instrument}: (\n')
+        for group in chords:
+            f.write(f'    (\n')
+            for suffix, frets in group.items():
+                f.write(f'      "{suffix}": "{frets}",\n')
+            f.write(f'    ),\n')
+        f.write(f'  ),\n')
     f.write(")\n")
